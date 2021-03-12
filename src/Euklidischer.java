b54b0b59a0ddd525988ce55e1827a3295c4de687
SebastianSauer <SebastianSauer@0-209-R13>
2021-03-12 10:58:48 +0100

    aaaaaa

--- src/Euklidischer.java
+++ src/Euklidischer.java
@@ -1,14 +1,18 @@
+import java.util.Random;
 public class Euklidischer {
-	static int a=8;
-	static int b=3;
+	static Random r=new Random();
+	static long a=r.nextInt(2143000000);
+	static long b=r.nextInt(2143000000);
 	
-	static int ggT(int a, int b) {
-		int r=a%b;
+	static long ggT(long a, long b) {
+		long r=a%b;
 		if(r==0)return b;
 		return ggT(b,r);
 	}
 	
 	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println(b);
 		System.out.println(ggT(a,b));
 	}
 }
